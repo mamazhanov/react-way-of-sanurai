0607cc80fe4f6e7de84eb0019a5fb0c8cf4b0584
Elmurat <elmurat.mamajanov@gmail.com>
2021-01-04 12:40:16 +0600

    DAL, axios.create(), ООП это круто!

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+        "API-KEY": "e9f77a73-7b1d-438b-9e96-62fe289ce549"
+    }
+})
+
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10){
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data;
+            });
+    }
+
+}
+
