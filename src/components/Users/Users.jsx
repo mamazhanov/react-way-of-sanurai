e90327e426c1ce9ba6915b68a15a743cd526c7f8
Elmurat <elmurat.mamajanov@gmail.com>
2021-01-07 13:23:04 +0600

    follow/unfollow Кнопка disablet

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -28,7 +28,8 @@ const Users = (props) => {
                                 </NavLink>
                                 <div>
                                     {u.followed
-                                        ? <button onClick={() => {
+                                        ? <button disabled={props.followingInProgress.some( id => id === u.id)} onClick={() => {
+                                            props.toggleFollowingProgress(true, u.id);
                                             axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
                                                 withCredentials: true,
                                                 headers: {
@@ -39,10 +40,12 @@ const Users = (props) => {
                                                     if (response.data.resultCode == 0) {
                                                         props.unfollow(u.id)
                                                     }
+                                                    props.toggleFollowingProgress(false,u.id);
                                                 });
                                         }}>Unfollow</button>
 
-                                        : <button onClick={() => {
+                                        : <button disabled={props.followingInProgress.some( id => id === u.id)} onClick={() => {
+                                            props.toggleFollowingProgress(true, u.id);
                                             axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
                                                 withCredentials: true,
                                                 headers: {
@@ -53,6 +56,7 @@ const Users = (props) => {
                                                    if (response.data.resultCode == 0) {
                                                        props.follow(u.id)
                                                    }
+                                                    props.toggleFollowingProgress(false, u.id);
                                                 });
                                         }}>Follow</button>}
                                 </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
-import {follow, setCurrentPage, setTotalUsersCount, setUsers, toggleIsFetching, unfollow} from "../../redux/users-reducer";
+import {
+    follow,
+    setCurrentPage,
+    setTotalUsersCount,
+    setUsers,
+    toggleFollowingProgress,
+    toggleIsFetching,
+    unfollow
+} from "../../redux/users-reducer";
 import {usersAPI} from "../../api/api";
 
 class UsersContainer extends React.Component {
@@ -35,7 +43,8 @@ class UsersContainer extends React.Component {
                        follow={this.props.follow}
                        currentPage={this.props.currentPage}
                        onPageChanged={this.onPageChanged}
-                       isFetching={this.props.isFetching}
+                       toggleFollowingProgress={this.props.toggleFollowingProgress}
+                       followingInProgress={this.props.followingInProgress}
                 />
             </div>
         )
@@ -48,10 +57,11 @@ let mapStateToProps = (state) => {
         pageSize: state.usersPage.pageSize,
         totalUsersCount: state.usersPage.totalUsersCount,
         currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching
+        isFetching: state.usersPage.isFetching,
+        followingInProgress: state.usersPage.followingInProgress
     }
 }
 
 export default connect(mapStateToProps, {
-    follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching
+    follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, toggleIsFetching, toggleFollowingProgress
 })(UsersContainer);
